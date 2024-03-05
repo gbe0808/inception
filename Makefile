0bc3f6a064e02f6a2e3393e01b39589832b62255
sunwoo <gbe0808@gmail.com>
2024-03-05 19:37:18 +0900

    good

--- Makefile
+++ Makefile
@@ -9,10 +9,8 @@ down :
 clean	:
 	cd srcs && docker compose down --rmi all -v --remove-orphans
 
-fclean	:
-#	docker stop -t 1 $(docker ps -q)
-	cd srcs && docker compose down --rmi all -v --remove-orphans
-	rm -rf /home/sunwsong/data/wpvol
-	rm -rf /home/sunwsong/data/dbvol
+fclean: clean
+	rm -rf /home/sunwsong/data
+	docker system prune --volumes --all --force
 
 re	: clean up
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+sleep 1
+
+cd /var/www/html
+
 wp core download --path=/var/www/html --locale=ko_KR --allow-root
-sleep 5
 wp config create --path=/var/www/html --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=$DB_HOST --locale=ko_KR --allow-root
-# wp core install --path=/var/www/html --url=https://sunwsong.42.fr --title=Title --admin_user=hyper --admin_password=$WP_ADMIN_PASSWD --admin_email=gbe0808@naver.com --allow-root
-# wp user create sunwsong user371302@naver.com --role=author --user_pass=$WP_USER_PASSWD --path=/var/www/html --allow-root
+wp core install --path=/var/www/html --url=https://sunwsong.42.fr --title=Title --admin_user=hyper --admin_password=$WP_ADMIN_PASSWD --admin_email=gbe0808@naver.com --allow-root
+wp user create sunwsong user371302@naver.com --role=author --user_pass=$WP_USER_PASSWD --path=/var/www/html --allow-root
 
 chown -R www-data:www-data /var/www/html
 chmod -R 774 /var/www/html
 
-# php-fpm81 -F
-/bin/sh
+php-fpm81 -F
